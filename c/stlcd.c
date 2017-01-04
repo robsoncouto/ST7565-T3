a7fe97d6c34c0e4629add44b5bb784f6b010cb0d
Robson <robsoncouto@outlook.com>
2017-01-03 23:43:15 -0300

    Works fine, still some compile warnings though

--- c/stlcd.c
+++ c/stlcd.c
@@ -217,16 +217,7 @@ void setup(void) {
 
     drawstring(buffer, 0, 0, "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.");
     write_buffer(buffer);
-    //  _delay_ms(2500);
-    //clear_buffer(buffer);
-
-    //testdrawbitmap(buffer, logo16_glcd_bmp, 16, 16);
-    //write_buffer(buffer);
-    //_delay_ms(2500);
-    //clear_buffer(buffer);
-
     //LED_PORT &= ~_BV(LED);
-    //write_buffer(buffer);
 }
 
 #define NUMFLAKES 10
