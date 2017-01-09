8c68854ee19c180bb6736b58096365eb610e57d0
Robson <robsoncouto@outlook.com>
2017-01-09 15:44:46 -0300

    Added a main file. See diff for details

--- c/stlcd.h
+++ c/stlcd.h
@@ -16,7 +16,7 @@
 
 #define CMD_SET_ALLPTS_NORMAL 0xA4
 #define CMD_SET_ALLPTS_ON  0xA5
-#define CMD_SET_BIAS_9 0xA2 
+#define CMD_SET_BIAS_9 0xA2
 #define CMD_SET_BIAS_7 0xA3
 
 #define CMD_RMW  0xE0
@@ -38,6 +38,45 @@
 #define CMD_NOP  0xE3
 #define CMD_TEST  0xF0
 
+
+
+//Pin defines
+
+#define BLA_DDR DDRC
+#define BLA_PIN PINC
+#define BLA_PORT PORTC
+#define BLA 5
+
+#define SID_DDR DDRD
+#define SID_PIN PIND
+#define SID_PORT PORTD
+#define SID 1
+
+#define SCLK_DDR DDRD
+#define SCLK_PIN PIND
+#define SCLK_PORT PORTD
+#define SCLK 2
+
+#define A0_DDR DDRD
+#define A0_PIN PIND
+#define A0_PORT PORTD
+#define A0 3
+
+#define RST_DDR DDRD
+#define RST_PIN PIND
+#define RST_PORT PORTD
+#define RST 4
+
+#define CS_DDR DDRD
+#define CS_PIN PIND
+#define CS_PORT PORTD
+#define CS 5
+
+#define LED_DDR DDRD
+#define LED_PORT PORTD
+#define LED 6
+
+//function prototypes
 void spiwrite(uint8_t c);
 
 void setup(void);
@@ -54,4 +93,9 @@ void clear_buffer(uint8_t *buffer);
 void write_buffer(uint8_t *buffer);
 
 void testdrawbitmap(uint8_t *buff, const uint8_t *bitmap, uint8_t w, uint8_t h);
-
+void testdrawchar(uint8_t *buff);
+void testdrawcircle(uint8_t *buff);
+void testdrawline(uint8_t *buff);
+void testdrawrect(uint8_t *buff);
+void testfillrect(uint8_t *buff);
+void clear_screen(void);
